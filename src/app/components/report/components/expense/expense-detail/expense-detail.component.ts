39d33e20dff9ce4dd730fe8a0659f136928b1e30
lzf_ccsu <854047294@qq.com>
2019-03-19 18:05:01 +0800

    report style

--- src/app/components/report/components/expense/expense-detail/expense-detail.component.ts
+++ src/app/components/report/components/expense/expense-detail/expense-detail.component.ts
@@ -1,24 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { GridOptions } from 'ag-grid';
 @Component({
   selector: 'app-expense-detail',
   templateUrl: './expense-detail.component.html',
   styleUrls: ['./expense-detail.component.scss']
 })
 export class ExpenseDetailComponent implements OnInit {
+  gridOptions: GridOptions;
 
-  columnDefs = [
-    {headerName: 'Make', field: 'make' },
-    {headerName: 'Model', field: 'model' },
-    {headerName: 'Price', field: 'price'}
-];
-
-rowData = [
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 }
-];
-  constructor() { }
+  constructor() {
+    this.gridOptions = <GridOptions>{
+      enableColResize: true,
+      enableSorting: true,
+      columnDefs: [
+        { headerName: 'Make', field: 'make' },
+        { headerName: 'Model', field: 'model' },
+        { headerName: 'Price', field: 'price' }
+      ],
+      rowData: [
+        { make: 'Toyota', model: 'Celica', price: 35000 },
+        { make: 'Ford', model: 'Mondeo', price: 32000 },
+        { make: 'Porsche', model: 'Boxter', price: 72000 },
+        { make: 'Toyota', model: 'Celica', price: 35000 },
+        { make: 'Ford', model: 'Mondeo', price: 32000 },
+        { make: 'Porsche', model: 'Boxter', price: 72000 }
+      ]
+    };
+  }
 
   ngOnInit() {
   }
